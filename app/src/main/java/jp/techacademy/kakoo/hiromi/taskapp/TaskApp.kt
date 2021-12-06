eb667b0fc6c49cf1816398f3ba757639e916524f
bdriver6 <bdriver6@gmail.com>
2021-12-07 00:32:51 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/kakoo/hiromi/taskapp/TaskApp.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.kakoo.hiromi.taskapp
+
+import android.app.Application
+import io.realm.Realm
+
+class TaskApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
